24d2bf3ea566ebb7841f1089c5ee6a0b1f2b677e
kidomaru <kingzakid@gmail.com>
2017-04-17 18:33:41 +0200

    Add multi-account feature

--- app/javascript/tab.js
+++ app/javascript/tab.js
@@ -1,3 +1,30 @@
-/**
- * Created by kidomaru on 17/04/2017.
- */
+const TabGroup = require("electron-tabs");
+let tabGroup = new TabGroup({newTab:{
+    title: "Dofus Tab",
+    src: "./game.html",
+    active: true,
+    visible: true
+}});
+
+tabGroup.on("tab-added", (tab, tabGroup) => {
+    tab.webview.setAttribute("useragent", "Mozilla/5.0 (Linux; Android 6.0; FEVER Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
+    tab.webview.setAttribute("preload", "./injector.js");
+});
+
+function newGameTab() {
+    let newTab = tabGroup.addTab({
+        title: "Deskofus",
+        src: "./game.html",
+        active: true,
+        visible: true
+    }).webview.setAttribute("useragent", "Mozilla/5.0 (Linux; Android 6.0; FEVER Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
+}
+
+//Add a new tab
+/*document.getElementsByClassName('etabs-tab-button-new')[0].onclick = function() {
+ newGameTab();
+ };*/
+
+module.exports = {
+    newGameTab: newGameTab()
+};
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let windows = [];
 function newGameWindow() {
     let window = new BrowserWindow({width: 1128, height: 649,});
 
-    window.loadURL("file://" + __dirname + "/app/game.html", {
+    window.loadURL("file://" + __dirname + "/app/tab.html", {
         userAgent: "Mozilla/5.0 (Linux; Android 6.0; FEVER Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36"
     });
 
